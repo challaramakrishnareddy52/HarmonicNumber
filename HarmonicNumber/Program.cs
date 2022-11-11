9988f067cfe1145afce565d86f3ae89ae937b5b7
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-11 23:42:45 +0530

    Harmonic Number

---
+++ HarmonicNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HarmonicNumber;
+
+     class program
+     {
+
+    static double nthHarmonic(int N)
+    {
+        float harmonic = 1;
+
+        for (int i = 2; i <= N; i++)
+        {
+            harmonic += (float)1 / i;
+        }
+
+        return harmonic;
+    }
+    static public void Main()
+    {
+        int N = 0;
+
+        Console.Write(nthHarmonic(N));
+    }
+}
+
+
